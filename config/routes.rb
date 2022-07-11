ede0f0d56613c29dc7329a733712289f9355bc3a
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-11 05:12:15 -0700

    created links

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/coffeeshops', to: 'coffeeshops#index'
+  get '/coffeeshops/:id', to: 'coffeeshops#show'
+
+  get '/baristas', to: 'baristas#index'
+  get '/baristas/:id', to: 'baristas#show'
+  get '/coffeeshops/:id/baristas', to: 'coffeeshop_baristas#index'
+
 end
