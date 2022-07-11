123fea0b05b3c10880c9d8866f0e4b4df1010334
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-11 05:11:28 -0700

    added test objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Coffeeshop.destroy_all
+Barista.destroy_all
+
+coffeeshop_1 = Coffeeshop.create( name: 'Cafe Olay',
+                                  capacity: 30,
+                                  outdoor_seating: true)
+
+coffeeshop_2 = Coffeeshop.create( name: 'Gentle Beans',
+                                  capacity: 20,
+                                  outdoor_seating: false)
+
+employee_1 = coffeeshop_1.baristas.create(name: 'Margarita',
+                                         is_full_time: false,
+                                         hours_worked: 20)
+
+employee_2 = coffeeshop_1.baristas.create(name: 'Diane',
+                                          is_full_time: true,
+                                          hours_worked: 32)
+
+employee_3 = coffeeshop_2.baristas.create(name: 'Marco',
+                                          is_full_time: true,
+                                          hours_worked: 36)
+
+employee_4 = coffeeshop_2.baristas.create(name: 'Rosie',
+                                           is_full_time: false,
+                                           hours_worked: 24)
