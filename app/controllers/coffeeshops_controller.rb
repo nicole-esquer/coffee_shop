027fa8224640f35e62bcdff5e50542639583cba3
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-11 05:10:57 -0700

    added methods

--- app/controllers/coffeeshops_controller.rb
+++ app/controllers/coffeeshops_controller.rb
@@ -1,8 +1,9 @@
 class CoffeeshopsController < ApplicationController
    def index
-      @coffeeshops = Coffeeshop.all
+      @coffeeshops = Coffeeshop.order_created_at_desc
    end
 
    def show
+      @coffeeshop = Coffeeshop.find(params[:id])
    end
 end
