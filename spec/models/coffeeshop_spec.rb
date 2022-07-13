ddb4fc4e1e6c209115f2c939b1b49a86d34ad04d
Nicole Esquer <nicole.esquer5@gmail.com>
2022-07-13 16:09:13 -0700

    model testing for coffeeshop

--- spec/models/coffeeshop_spec.rb
+++ spec/models/coffeeshop_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Coffeeshop, type: :model do
   describe 'validations' do
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :capacity }
+    it { should validate_presence_of (:name) }
+    it { should validate_presence_of (:capacity) }
   end
 
    describe 'relationships' do
-  	it { should have_many :employees }
+  	xit { should have_many (:baristas) }
   end
 end
